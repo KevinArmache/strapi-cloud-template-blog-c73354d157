939cdd79b380e588552ef3cba59a036bb06913cc
Kevin Armache <kevinarmache@gmail.com>
2026-01-29 18:08:51 +0100

    fix

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -542,6 +542,7 @@ export interface ApiProjetProjet extends Struct.CollectionTypeSchema {
       Schema.Attribute.Private;
     description: Schema.Attribute.Text;
     genre: Schema.Attribute.Relation<'manyToOne', 'api::genre.genre'>;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localisation: Schema.Attribute.Text;
     localizations: Schema.Attribute.Relation<
